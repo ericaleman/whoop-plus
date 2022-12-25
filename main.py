b9a3e368445b5a07818e1fd2228e2b1506fd2508
ericaleman <mail@ericaleman.com>
2022-12-25 16:56:48 -0500

    Updates to readme

--- main.py
+++ main.py
@@ -27,41 +27,6 @@ def graph_trends(metric):
 
 if __name__ == "__main__":
 
-	"""
-	Allows you to graph trends for any of the following metrics:
-
-		strain,
-		calories,
-
-		average_heart_rate,
-		max_heart_rate,
-		resting_heart_rate,
-
-		respiratory_rate,
-		hrv_rmssd_milli, 
-		spo2_percentage,
-
-		skin_temp_celsius,
-
-		total_in_bed_time_milli,
-		total_awake_time_milli,
-		total_no_data_time_milli,
-		total_light_sleep_time_milli, 
-		total_slow_wave_sleep_time_milli, 
-		total_rem_sleep_time_milli, 
-		sleep_cycle_count, 
-
-		need_from_baseline_milli,
-		need_from_sleep_debt_milli,
-		need_from_recent_strain_milli,
-		need_from_recent_nap_milli,
-		
-		sleep_performance_percentage,
-		sleep_consistency_percentage,
-		sleep_efficiency_percentage
-
-	"""
-	
 	# Open OAuth2 session for WHOOP API
 	load_dotenv()
 	USERNAME = os.getenv("USERNAME")
